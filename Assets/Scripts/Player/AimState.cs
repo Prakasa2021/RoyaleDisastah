ba2f9f23c0228396faa2976031b00d76ed085077
M T Y P <92069013+Prakasa2021@users.noreply.github.com>
2024-03-03 23:27:19 +0700

    Aiming and Shooting

--- Assets/Scripts/Player/AimState.cs
+++ Assets/Scripts/Player/AimState.cs
@@ -2,20 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Cinemachine;
-using System.Numerics;
+using System.Net.NetworkInformation;
 
 public class AimState : MonoBehaviour
 {
     public AxisState xAxis, yAxis;
     [SerializeField] Transform camFollow;
     [SerializeField] float mouseSense;
-    [SerializeField] float aimSmoothSpeed = 20f;
-    [SerializeField] LayerMask aimMask;
     public CinemachineVirtualCamera vCam;
     // public float adsFov = 40f;
     // public float hipFov;
-    public float currentFov;
+    public float normalFov;
+    public float aimFov;
     public float fovSmoothSpeed = 10f;
+    public Transform aimPos;
+    [SerializeField] float aimSmoothSpeed = 20f;
+    [SerializeField] LayerMask aimMask;
 
     void Start()
     {
@@ -29,21 +31,26 @@ public class AimState : MonoBehaviour
         yAxis.Value -= Input.GetAxisRaw("Mouse Y") * mouseSense;
         yAxis.Value = Mathf.Clamp(yAxis.Value, -80, 80);
 
-        vCam.m_Lens.FieldOfView = Mathf.Lerp(vCam.m_Lens.FieldOfView, currentFov, fovSmoothSpeed * Time.deltaTime);
+        float targetFov = Input.GetMouseButton(1) ? aimFov : normalFov;
+        ChangeFov(targetFov);
 
-        if (Input.GetMouseButton(1))
+        Vector2 screenCenter = new(Screen.width / 2, Screen.height / 2);
+        Ray ray = Camera.main.ScreenPointToRay(screenCenter);
+
+        if (Physics.Raycast(ray, out RaycastHit hit, Mathf.Infinity, aimMask))
         {
-            currentFov = 25f;
-        }
-        else
-        {
-            currentFov = 50f;
+            aimPos.position = Vector3.Lerp(aimPos.position, hit.point, aimSmoothSpeed * Time.deltaTime);
         }
     }
 
     void LateUpdate()
     {
-        camFollow.localEulerAngles = new UnityEngine.Vector3(yAxis.Value, camFollow.localEulerAngles.y, camFollow.localEulerAngles.z);
-        transform.eulerAngles = new UnityEngine.Vector3(transform.eulerAngles.x, xAxis.Value, transform.eulerAngles.z);
+        camFollow.localEulerAngles = new Vector3(yAxis.Value, camFollow.localEulerAngles.y, camFollow.localEulerAngles.z);
+        transform.eulerAngles = new Vector3(transform.eulerAngles.x, xAxis.Value, transform.eulerAngles.z);
+    }
+
+    public void ChangeFov(float currentFov)
+    {
+        vCam.m_Lens.FieldOfView = Mathf.Lerp(vCam.m_Lens.FieldOfView, currentFov, fovSmoothSpeed * Time.deltaTime);
     }
 }
---
+++ Assets/Scripts/Weapon/Pistol/PistolManager.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PistolManager : MonoBehaviour
+{
+    [SerializeField] float fireRate;
+    [SerializeField] bool semiAuto;
+    float fireRateTimer;
+    [SerializeField] GameObject bulletPrefabs;
+    [SerializeField] Transform BulletSpawnPos;
+    [SerializeField] float bulletSpeed = 40f;
+    [SerializeField] int bulletPerShot;
+    public AimState aim;
+
+    void Start()
+    {
+        aim = GameObject.FindGameObjectWithTag("Player").GetComponent<AimState>();
+        fireRateTimer = fireRate;
+    }
+
+    void Update()
+    {
+        if (transform.parent != null && ShouldFire())
+        {
+            // var bullet = Instantiate(bulletPrefabs, BulletSpawnPos.position, BulletSpawnPos.rotation);
+            // bullet.GetComponent<Rigidbody>().velocity = BulletSpawnPos.forward * bulletSpeed;
+            Fire();
+        }
+    }
+
+    bool ShouldFire()
+    {
+        fireRateTimer += Time.deltaTime;
+        if (fireRateTimer < fireRate) return false;
+        if (semiAuto && Input.GetKeyDown(KeyCode.Mouse0)) return true;
+        if (!semiAuto && Input.GetKey(KeyCode.Mouse0)) return true;
+        return false;
+    }
+
+    void Fire()
+    {
+        fireRateTimer = 0;
+        BulletSpawnPos.LookAt(aim.aimPos);
+
+        for (int i = 0; i < bulletPerShot; i++)
+        {
+            GameObject currentBullet = Instantiate(bulletPrefabs, BulletSpawnPos.position, BulletSpawnPos.rotation);
+            currentBullet.GetComponent<Rigidbody>().AddForce(BulletSpawnPos.forward * bulletSpeed, ForceMode.Impulse);
+        }
+    }
+
+}
